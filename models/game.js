999705d0d5b38ce30b1936f4e1beb6126fc3802f
floris09 <florismeininger@gmail.com>
2017-11-08 12:13:38 +0100

    fix bug

--- models/game.js
+++ models/game.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose
 
 const gameSchema = new Schema({
   fields: [{ type: Number, default: 0 }],
-  players: [{ type: Schema.Types.ObjectId, ref: 'users' }],
+  players: [{userId: { type: Schema.Types.ObjectId, ref: 'users' }}],
   turn: { type: Number, default: 0 }, // player index
   started: { type: Boolean, default: false },
   winnerId: { type: Schema.Types.ObjectId, ref: 'users' },
